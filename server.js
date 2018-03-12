312e4b7a2a8f4487c62cbf961e0d668d564dedcd
Douglas Figueiredo <douglasarts@gmail.com>
2018-03-12 17:08:08 -0300

    Refatoração no server.js

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
+//Install express server
+const express = require('express');
+const app = express();
+const path = require('path');
 
-connect().use(
-    serveStatic("../angularjs")
-).listen(5000);
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+  });
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
