f21059cd110adacba839a017b8738a376c5353b5
Douglas Figueiredo <douglasarts@gmail.com>
2018-04-02 21:43:20 -0300

    Finalização da área de notificação

--- src/app/notificacoes/notificacoes.component.ts
+++ src/app/notificacoes/notificacoes.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Bd } from '../bd.service'
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireDatabaseModule, AngularFireList, AngularFireDatabase } from 'angularfire2/database';
+import * as firebase from 'firebase'
+import { Auth } from '../auth.service';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/Rx';
 
@@ -12,15 +13,30 @@ import 'rxjs/Rx';
 })
 export class NotificacoesComponent implements OnInit {
 
-  public contadorMsg: any
+  public contadorMsg: any = 0
   public notificacoes: Observable<any[]>
-  // item: Observable<any>;
+  public email: any
+  public notificacao: any
 
-  constructor(db: AngularFireDatabase) { 
-    // this.contador()
-    this.notificacoes = db.list('notificacoes').valueChanges();
-    this.notificacoes.subscribe(result => { this.contadorMsg = result.length});
-    this.notificacoes.subscribe(result => { console.log( result)});
+  itemsRef: AngularFireList<any>;
+
+  constructor(private db: AngularFireDatabase, private auth: Auth) { 
+
+    firebase.auth().onAuthStateChanged((user) => {
+      this.email = user.email
+      
+      this.itemsRef = db.list(`notificacoes/${btoa(this.email)}`);
+      
+      this.notificacoes = db.list(`notificacoes/${btoa(this.email)}`).valueChanges()
+      this.notificacoes.subscribe(result => { 
+        this.contadorMsg = result.length
+        result.forEach((resultItem) => {
+          if(resultItem.lida == true){
+            this.contadorMsg --
+          }
+        })
+      });
+    })
   }
 
   ngOnInit() {
@@ -31,9 +47,10 @@ export class NotificacoesComponent implements OnInit {
     //   // console.log(intervalo)
     // })
   }
-  public notificacaoLida(event):void{
-    // console.log(htm)
-  }
 
+  public notificacaoLida(notificacao):void{
+    this.notificacao = (<HTMLInputElement>notificacao.target).value
+    this.itemsRef.update(this.notificacao, { lida: true });
+  }
 
 }
